040627ae0cd8ee1537dd892cedec6b48a08e95f4
Azwar <azwar@azfit.com>
2026-07-25 22:27:31 +0800

    chore(21b): delete 50 verified dead files (SheetsPanel + shadcn ui island + misc)

--- src/lib/index.ts
+++
@@ -1,4 +0,0 @@
-// Barrel export for lib utilities
-export { cn, formatElapsed, formatDate, formatNumber, calculateBMI, calculateBMR, calculateTDEE } from './utils';
-export { supabase, type SupabaseClient } from './supabase';
-export * from './storage';
